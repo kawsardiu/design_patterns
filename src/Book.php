263b4a317981ddeb490e44c9e1d652107ec3bdec
kawsar joy <kawsarjoydiu@gmail.com>
2017-07-06 20:16:04 +0600

    Finish adapter design petterns form Laracasts (#2)

---
+++ src/Book.php
@@ -0,0 +1,18 @@
+<?php 
+
+	namespace Acme;
+
+	class Book implements BookInterface
+	{
+		public function open()
+		{
+			var_dump("opening the book");
+		}
+
+		public function turnPage()
+		{
+			var_dump("Turing the page");
+		}
+	}
+
+ ?>
---
+++ src/Nook.php
@@ -0,0 +1,19 @@
+<?php 
+
+	namespace Acme;
+
+	class Nook implements eReaderInterface
+	{
+		public function turnOn()
+		{
+			var_dump("Turn the nook on");
+		}
+
+		public function pressNextButton()
+		{
+			var_dump("Press the next button on the nook");
+		}
+	}
+
+
+ ?>
